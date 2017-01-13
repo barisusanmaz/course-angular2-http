0be77a9149ebd923d8f7669d8502ff2efa985eaf
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-13 16:07:57 +0300

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {HttpService} from "./http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items: any[];
+  asyncString = this.httpService.getData();
 
+  constructor(private httpService: HttpService) {
+  }
+
+  ngOnInit() {
+    this.httpService.getData().subscribe(
+      (data) => console.log(data)
+    );
+  }
+
+  onClick(username: string, email: string) {
+    this.httpService.sendData({username: username, email: email}).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
+
+  onGetData() {
+    this.httpService.getOwnData().subscribe(
+      data => {
+        const myArray = [];
+        for(let key in data) {
+          myArray.push(data[key]);
+        }
+        this.items = myArray;
+      }
+    );
+  }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Response, Headers} from "@angular/http";
+import "rxjs/Rx";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class HttpService {
@@ -7,4 +9,32 @@ export class HttpService {
   constructor(private http: Http) {
   }
 
+  getData() {
+    return this.http.get('https://angular2-course-ee7bc.firebaseio.com/title.json')
+      .map(
+        (response: Response) => response.json()
+      );
+  }
+
+  sendData(user: any) {
+    const body = JSON.stringify(user);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post('https://angular2-course-ee7bc.firebaseio.com/data.json', body, {headers: headers})
+      .map((data: Response) => data.json())
+      .catch(this.handleError);
+  }
+
+  getOwnData() {
+    return this.http.get('https://angular2-course-ee7bc.firebaseio.com/data.json')
+      .map(
+        (response: Response) => response.json()
+      );
+  }
+
+  private handleError(error: any) {
+    console.log(error);
+    return Observable.throw(error.json());
+  }
 }
